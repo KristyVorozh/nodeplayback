6b313f6404b36d433b2c7193f2c1445d5ec774c3
kvorozh <kvorozhejkina@logicstars.ru>
2023-03-03 16:03:38 +0700

    fix length password

--- router/authRouter.js
+++ router/authRouter.js
@@ -6,7 +6,7 @@ const {check} = require("express-validator");
 router.post("/reg", [
     check('email', "Имя пользователя не может быть пустым").notEmpty(),
     check('username', "Имя пользователя не может быть пустым").notEmpty(),
-    check('password', "password need more 4 and less 10").isLength({min: 4, max: 10})
+    check('password', "password need more 4 and less 10").isLength({min: 6})
 ], controller.registration)
 router.post("/login", controller.login)
 
